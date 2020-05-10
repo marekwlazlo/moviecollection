1b6aef57dbfd57dd9b1d23191f9cfd73d6d21e7b
Marek <marek.wlazlo97@gmail.com>
2020-05-10 21:56:08 +0200

    v2

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,7 @@ function Search ({ handleInput, searching }) {
 		<section className="searchbox-wrap">
 			<input 
 				type="text" 
-				placeholder="enter the name of the film, for example hobbit" 
+				placeholder="enter the name of the film, for example superman" 
 				className="searchbox" 
 				onChange={handleInput}
 				onKeyPress={searching}
